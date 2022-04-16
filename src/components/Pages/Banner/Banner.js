a034513b06a8094fd46f0e29edc5c61442cb23ad
neel123go <ayounpaul1029@gmail.com>
2022-04-17 00:35:11 +0600

    banner slider completed

---
+++ src/components/Pages/Banner/Banner.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { Carousel } from 'react-bootstrap';
+import Pic1 from '../../../images/slider-1.jpg';
+import Pic2 from '../../../images/slider-2.jpg';
+import Pic3 from '../../../images/slider-3.jpg';
+import './Banner.css';
+
+const Banner = () => {
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    };
+
+    return (
+        <div>
+            <Carousel activeIndex={index} onSelect={handleSelect}>
+                <Carousel.Item>
+                    <img
+                        className="d-block img-fluid w-100"
+                        src={Pic1}
+                        alt="First slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block img-fluid w-100"
+                        src={Pic2}
+                        alt="Second slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block img-fluid w-100"
+                        src={Pic3}
+                        alt="Third slide"
+                    />
+                </Carousel.Item>
+            </Carousel>
+        </div>
+    );
+};
+
+export default Banner;
