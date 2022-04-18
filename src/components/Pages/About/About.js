71ec79d4d6f75aff4f0d82346e57d508882c951f
neel123go <ayounpaul1029@gmail.com>
2022-04-18 15:24:31 +0600

    about page added

--- src/components/Pages/About/About.js
+++ src/components/Pages/About/About.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import ProfileImg from '../../../images/About/profile.jpg';
+import './About.css';
 
 const About = () => {
     return (
-        <div className='container'>
+        <div className='container my-5'>
             <h2 className='text-center text-danger my-5'>About Me</h2>
+            <div className='d-flex p-5 w-75 mx-auto about rounded-3'>
+                <div className='d-flex justify-content-center align-items-center w-25'>
+                    <img className='w-100 rounded-circle' src={ProfileImg} alt="" />
+                </div>
+                <div className='w-75 ms-5 mt-4'>
+                    <h2>Ayoun Paul Neel</h2>
+                    <p className='mt-3'>I want to be a web developer. I believe that dreams don't work unless you do. So, to fulfill my dreams I always watch every single module attentively and do every assignment carefully and honestly which is given by Programming Hero</p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Pages/Blogs/Blogs.js
+++ src/components/Pages/Blogs/Blogs.js
@@ -11,7 +11,7 @@ const Blogs = () => {
                 <h3 className='mt-5'>Why are you using firebase? What other options do you have to implement authentication?</h3>
                 <p className='text-secondary'>I use Firebase's Firebase Authentication which allows users to sign in to my website using one or more sign-in methods, including email addresses and password sign-in and federated identifiers such as Google sign-in and Facebook login. I also use Firebase for hosting my website. Firebase Hosting is production-grade web content hosting for developers. With a single command, I can quickly deploy websites and serve both static and dynamic content to a global CDN (content delivery network). I have many options for implementing authentication such as Purse, Back4app, AWS Amplify, Kuzal, Couchbase, etc.</p>
 
-                <h3 className='mt-5'> What other services does firebase provide other than authentication</h3>
+                <h3 className='mt-5'> What other services does firebase provide other than authentication?</h3>
                 <p className='text-secondary pb-3'>Firebase is a full package that can help in developing your mobile or web applications faster with fewer resources and more efficiency. There are many services that Firebase provides other than authentication, Most useful of them are: 1. Cloud Firestore, 2. Cloud Functions, 3. Hosting, 4. Cloud Storage, 5. Google Analytics, 6. Predictions, 7. Cloud Messaging, 8. Dynamic Links, 9. Remote Config etc.</p>
             </div>
         </div>
